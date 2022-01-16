b39b8807460134a0a69df6fb5f595da82ffbd43d
ArishMada <arishmadataly@gmail.com>
2022-01-16 13:48:51 +0300

    update

--- Escapism.js
+++ Escapism.js
@@ -23,3 +23,19 @@ window.addEventListener("scroll",function(event){
       SemiMenu.className='gone'
     }
   })
+
+  
+var item = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < item.length; i++) {
+  item[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
