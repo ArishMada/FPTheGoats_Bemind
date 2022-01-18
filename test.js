d2ef49546ef3c341aba30f9f38c6f4726d3c3647
ArishMada <arishmadataly@gmail.com>
2022-01-18 21:10:27 +0300

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-var box = document.querySelectorAll('.box');
-
-for (let i = 0; i<box.length; i++){
-    box[i].addEventListener( 'click', function() {
-        box[i].classList.toggle('is-flipped');
-      });
-}
