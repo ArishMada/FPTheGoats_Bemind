35b6e212c401b800e88ab136cfa1891a6b21d6c0
ArishMada <arishmadataly@gmail.com>
2022-01-18 08:16:47 +0300

    correcting mistakes and adding schedule for physical wb

---
+++ Physical wb.js
@@ -0,0 +1,20 @@
+// the semi-circular nav bar only appears when we have scrolled down 200
+window.addEventListener("scroll",function(event){
+    var scroll= this.scrollY;
+    console.log(scroll);
+    var SemiMenu= document.getElementById('NavScroll')
+    if (scroll>80){
+      SemiMenu.className='semicircle'
+    } else{
+      SemiMenu.className='gone'
+    }
+  })
+
+  var box = document.querySelectorAll('.box');
+
+  for (let i = 0; i<box.length; i++){
+      box[i].addEventListener( 'click', function() {
+          box[i].classList.toggle('is-flipped');
+        });
+  }
+  
